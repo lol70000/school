cf27afc0319fa99df5257f0df5f8d92a4e4ad4fe
Tobias Luchsinger S Kantonsschule Glarus <tobias.luchsinger@stud.schulegl.ch>
2022-09-08 13:38:00 +0200

    add file

---
+++ dez_bin.py
@@ -0,0 +1,10 @@
+def dez_zu_bin(dez_zahl):
+    ans = bin(dez_zahl,).replace("0b","")
+    print(ans)
+
+def bin_zu_dez(bin_zahl):
+    ans =  int(bin_zahl,2)
+    print(ans)
+
+bin_zu_dez("1011") 
+dez_zu_bin(11)
